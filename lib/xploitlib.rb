c9b186352121d11e0e6dc19e51c1f8324f5a0617
Chihiro Hasegawa <pgm3rdlinuxor1000@gmail.com>
2017-11-07 16:07:50 +0900

    update xploitlib.rb

--- lib/xploitlib.rb
+++ lib/xploitlib.rb
@@ -2,6 +2,9 @@
 require "xploitlib/version"
 require "xploitlib/sock"
 require "xploitlib/util"
+require "xploitlib/shellcode"
+require "xploitlib/libc"
 
 include Xploitlib
 include ::Pack
+include ::Libc
